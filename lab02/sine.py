503d29752fa5825de5a9a14f39a68f77453f16e2
cwalstra <cjwalstra@gmail.com>
2019-02-08 17:44:51 -0500

    Added random restarts to sine.py

--- lab02/sine.py
+++ lab02/sine.py
@@ -38,13 +38,32 @@ class AbsVariant(Problem):
 # This creates a 2D hill function with a single maximum value and runs both local search algorithms.
 maximum = 30
 initial = randrange(0, maximum)  # Pick a random starting value for x.
-p = AbsVariant(initial, maximum, delta=1.0)  # Create an Abs variant problem, specifying the delta step value.
+bestHillClimbX = 0
+bestSimAnnX = 0
+hillList = []
+annealList = []
+for i in range(100):
+    initial = randrange(0, maximum)  # Pick a random starting value for x.
+    p = AbsVariant(initial, maximum, delta=1.0)  # Create an Abs variant problem, specifying the delta step value.
+
+    # Solve the problem using hill-climbing.
+    hill_solution = hill_climbing(p)
+    hillList.append(p.value(hill_solution))
+    if p.value(hill_solution) > p.value(bestHillClimbX):
+        bestHillClimbX = hill_solution
+
+    # Solve the problem using simulated annealing.
+    annealing_solution = simulated_annealing(p, exp_schedule(k=20, lam=0.005, limit=1000))
+    annealList.append(p.value(annealing_solution))
+    if p.value(annealing_solution) > p.value(bestSimAnnX):
+        bestSimAnnX = annealing_solution
+
+
+hillAvg = sum(hillList)/float(len(hillList))
+annealAvg = sum(annealList)/float(len(annealList))
+
 print('Initial                      x: ' + str(p.initial) + '\t\tvalue: ' + str(p.value(initial)))
-
-# Solve the problem using hill-climbing.
-hill_solution = hill_climbing(p)
-print('Hill-climbing solution       x: ' + str(hill_solution) + '\tvalue: ' + str(p.value(hill_solution)))
-
-# Solve the problem using simulated annealing.
-annealing_solution = simulated_annealing(p, exp_schedule(k=20, lam=0.005, limit=1000))
-print('Simulated annealing solution x: ' + str(annealing_solution) + '\tvalue: ' + str(p.value(annealing_solution)))
+print('Hill-climbing solution       x: ' + str(bestHillClimbX) + '\tvalue: ' + str(p.value(bestHillClimbX)))
+print('Simulated annealing solution x: ' + str(bestSimAnnX) + '\tvalue: ' + str(p.value(bestSimAnnX)))
+print(f'Hill average: {hillAvg}')
+print(f"Annealing average {annealAvg}")
