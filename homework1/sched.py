173f59af56d6ac9955a21ce2cf1f4bba4ae0aa38
cwalstra <cjwalstra@gmail.com>
2019-02-18 20:21:01 -0500

    Major Progress; still needs refinement

--- homework1/sched.py
+++ homework1/sched.py
@@ -2,28 +2,23 @@
 Create a mini schedule for the CS department to use for access to SB354 and SB372
 '''
 
-from csp import Zebra, min_conflicts, backtracking_search, AC3
+from csp import CSP, min_conflicts, backtracking_search, AC3, parse_neighbors
 from search import depth_first_graph_search
 
 # Schedule Problem Class
 def Schedule():
-    Colors = 'Red Yellow Blue Green Ivory'.split()
-    Pets = 'Dog Fox Snails Horse Zebra'.split()
-    Drinks = 'OJ Tea Coffee Milk Water'.split()
-    Countries = 'Englishman Spaniard Norwegian Ukranian Japanese'.split()
-    Smokes = 'Kools Chesterfields Winston LuckyStrike Parliaments'.split()
-    variables = Colors + Pets + Drinks + Countries + Smokes
+    Profs = 'dschuurman adams vnorman kvlinden'.split()
+    Classes = 'cs108 cs112 cs212 cs214 cs300 cs344'.split()
+    Times = 'mwf800 mwf900 mwf1030 mwf1130'.split()
+    Rooms = 'sb354 sb372'.split()
+    variables = Profs + Times + Rooms
     domains = {}
     for var in variables:
-        domains[var] = list(range(1, 6))
-    domains['Norwegian'] = [1]
-    domains['Milk'] = [3]
-    neighbors = parse_neighbors("""Englishman: Red;
-                Spaniard: Dog; Kools: Yellow; Chesterfields: Fox;
-                Norwegian: Blue; Winston: Snails; LuckyStrike: OJ;
-                Ukranian: Tea; Japanese: Parliaments; Kools: Horse;
-                Coffee: Green; Green: Ivory""", variables)
-    for type in [Colors, Pets, Drinks, Countries, Smokes]:
+        domains[var] = Classes
+
+    neighbors = parse_neighbors('dschuurman: ; adams: ; vnorman: ; kvlinden: ; '
+                                'mwf800: ; mwf900: ; mwf1030: ; mwf1130: ; sb354: ; sb372: ', variables)
+    for type in [Profs, Times, Rooms]:
         for A in type:
             for B in type:
                 if A != B:
@@ -31,66 +26,30 @@ def Schedule():
                         neighbors[A].append(B)
                     if A not in neighbors[B]:
                         neighbors[B].append(A)
+    print(neighbors)
 
-    def zebra_constraint(A, a, B, b, recurse=0):
+    def schedule_constraints(A, a, B, b, recurse=0):
         same = (a == b)
-        next_to = abs(a - b) == 1
-        if A == 'Englishman' and B == 'Red':
-            return same
-        if A == 'Spaniard' and B == 'Dog':
-            return same
-        if A == 'Chesterfields' and B == 'Fox':
-            return next_to
-        if A == 'Norwegian' and B == 'Blue':
-            return next_to
-        if A == 'Kools' and B == 'Yellow':
-            return same
-        if A == 'Winston' and B == 'Snails':
-            return same
-        if A == 'LuckyStrike' and B == 'OJ':
-            return same
-        if A == 'Ukranian' and B == 'Tea':
-            return same
-        if A == 'Japanese' and B == 'Parliaments':
-            return same
-        if A == 'Kools' and B == 'Horse':
-            return next_to
-        if A == 'Coffee' and B == 'Green':
-            return same
-        if A == 'Green' and B == 'Ivory':
-            return a - 1 == b
+
         if recurse == 0:
-            return zebra_constraint(B, b, A, a, 1)
-        if ((A in Colors and B in Colors) or
-                (A in Pets and B in Pets) or
-                (A in Drinks and B in Drinks) or
-                (A in Countries and B in Countries) or
-                (A in Smokes and B in Smokes)):
+            return schedule_constraints(B, b, A, a, 1)
+        if ((A in Profs and B in Profs) or
+                (A in Times and B in Times) or
+                (A in Rooms and B in Rooms)):
             return not same
         raise Exception('error')
-    return CSP(variables, domains, neighbors, zebra_constraint)
+    return CSP(variables, domains, neighbors, schedule_constraints)
 
-
-def solve_zebra(algorithm=min_conflicts, **args):
-    z = Zebra()
-    ans = algorithm(z, **args)
-    for h in range(1, 6):
-        print('House', h, end=' ')
-        for (var, val) in ans.items():
-            if val == h:
-                print(var, end=' ')
-        print()
-    return ans['Zebra'], ans['Water'], z.nassigns, ans
 
 def print_solution(result):
     """A CSP solution printer copied from csp.py."""
-    for h in range(1, 6):
-        print('House', h)
+    for Class in 'cs108 cs112 cs212 cs214 cs300 cs344'.split():
+        print('Class', Class)
         for (var, val) in result.items():
-            if val == h: print('\t', var)
+            if val == Class: print('\t', var)
 
 
-puzzle = Zebra()
+puzzle = Schedule()
 
 # result = depth_first_graph_search(puzzle)
 # result = AC3(puzzle)
